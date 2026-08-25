4e850cd6e0c917d44b49d39fafb51770c00bef03
Luis Diaz <Octobr26@users.noreply.github.com>
2026-08-24 23:31:42 -0700

    feat(nvim): use Gruvbox Dark

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = {
+      contrast = "",
+      terminal_colors = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
